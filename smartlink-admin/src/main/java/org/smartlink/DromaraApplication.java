9b8a4b72a50d38990a099f41486d5051f5d5c4e4
ShiDunKai <1658554876@qq.com>
2025-01-09 09:55:38 +0800

    提交common用途

--- smartlink-admin/src/main/java/org/smartlink/DromaraApplication.java
+++ smartlink-admin/src/main/java/org/smartlink/DromaraApplication.java
@@ -1,5 +1,6 @@
 package org.smartlink;
 
+import org.smartlink.system.service.ISysUserService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup;
