8a08e4f6c1b371a84a48a8329d0c6aedbe3f695f
mengdongnan <2149003264@qq.com>
2025-08-12 16:18:27 +0800

    发票回写查询

---
+++ smartlink-modules/smartlink-business/src/main/java/org/smartlink/business/doman/vo/InvoiceWriteBackVo.java
@@ -0,0 +1,25 @@
+package org.smartlink.business.doman.vo;
+
+import lombok.Data;
+
+@Data
+public class InvoiceWriteBackVo {
+    private String amount;//金额
+    private String details;//详情
+    private String invoiceId;//发票id
+
+    public InvoiceWriteBackVo setInvoiceId(String invoiceId) {
+        this.invoiceId = invoiceId;
+        return this;
+    }
+
+    public InvoiceWriteBackVo setAmount(String amount) {
+        this.amount = amount;
+        return this;
+    }
+
+    public InvoiceWriteBackVo setDetails(String details) {
+        this.details = details;
+        return this;
+    }
+}
