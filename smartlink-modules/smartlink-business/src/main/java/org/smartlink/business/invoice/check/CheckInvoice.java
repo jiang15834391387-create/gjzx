ff26c93d852b60c11ce8ca5aa0da2ffd036b359e
mengdongnan <2149003264@qq.com>
2025-08-21 17:28:31 +0800

    数电票修改

--- smartlink-modules/smartlink-business/src/main/java/org/smartlink/business/invoice/check/CheckInvoice.java
+++ smartlink-modules/smartlink-business/src/main/java/org/smartlink/business/invoice/check/CheckInvoice.java
@@ -121,7 +121,7 @@ public class CheckInvoice {
                     //数电票(增值税)/普通发票处理
                     if (invoiceType.equals(InvoiceConstants.DIGITAL_INVOICE_VAT_SPECIAL_CODE)||invoiceType.equals(InvoiceConstants.DIGITAL_INVOICE_ORDINARY_INVOICE_CODE)){
                         invoiceCheckParamDTO.setNumber(ocrInfos.getInvoiceNumber());
-                        invoiceCheckParamDTO.setTotal(ocrInfos.getTotalLowercase());
+                        invoiceCheckParamDTO.setTotal(ocrInfos.getInvoiceTotal());
                         invoiceCheckParamDTO.setDate(ocrInfos.getInvoiceDate());
                         invoiceCheckParamDTO.setType(filesInfo.getInvoice());
                         break;
